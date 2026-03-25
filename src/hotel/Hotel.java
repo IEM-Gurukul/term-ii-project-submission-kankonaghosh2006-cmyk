9cb53c05ca02389b928e5aa99fa8560239cfac6a
kankonaghosh2006-cmyk <kankonaghosh2006@gmail.com>
2026-03-25 18:58:31 +0530

    Implement room booking functionality

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -13,6 +13,20 @@ class Hotel {
     void viewRooms() {
         for (Room r : rooms) {
             r.display();
+            
+            
         }
     }
+    void bookRoom(Customer customer, int roomNumber) {
+    for (Room r : rooms) {
+        if (r.roomNumber == roomNumber && r.isAvailable) {
+            r.isAvailable = false;
+            Booking b = new Booking(customer, r);
+            bookings.add(b);
+            System.out.println("Room booked successfully!");
+            return;
+        }
+    }
+    System.out.println("Room not available!");
+}
 }
